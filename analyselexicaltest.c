d03ab81c7845dc63075340e1e0386c09a3380e74
Selma Mokhbat <mokhbatselma06@gmail.com>
2021-12-17 12:39:18 +0100

    affichage des mot cle&& id

--- analyselexicaltest.c
+++ analyselexicaltest.c
@@ -6,7 +6,7 @@
 void main(){
 
 int i,ic=0,m,cc=0,oc=0,mc=0,sp=0,j;
-char chaine[30],operators[30],identifiers[30],constants[30],motcle[30],separators[30];
+char chaine[500],operators[30],identifiers[30],constants[30],motcle[30],separators[30];
 
 printf(" TAPEZ LE CODE ICI : ");
 scanf("%[^\n]s",&chaine);
@@ -142,8 +142,10 @@ for(i=0;i<strlen(chaine);i++){
             operators[oc]='=';
             oc++;
         }
-        else if(chaine[i]==':='){
-            operators[oc]=':=';
+        else if((chaine[i]==':')&& (chaine[i+1]=='=')){
+            operators[oc]=':';
+            oc++;
+            operators[oc]='=';
             oc++;
         }
         else if(chaine[i]==';'){
@@ -172,7 +174,17 @@ for(i=0;i<strlen(chaine);i++){
 
     printf(" \n\n| identificateurs | ");
      for(j=0;j<ic;j++){
-        printf("%c ",identifiers[j]);
+        if((identifiers[j]=='t')&&(identifiers[j+1]=='e')&&(identifiers[j+2]=='s')&&(identifiers[j+3]=='t')){
+         printf(" test ");
+        } else if ((identifiers[j]=='f')&&(identifiers[j+1]=='i')&&(identifiers[j+2]=='b')){
+         printf(" fib ");
+        } else if (identifiers[j]=='i'){
+        printf(" i ");
+        } else if (identifiers[j]=='j'){
+        printf(" j ");
+        }else if (identifiers[j]=='n'){
+        printf(" n ");
+        }
      }
      if(ic==0) printf(" C EST VIDE!");
     printf("\n\n  | constantes | ");
@@ -181,11 +193,39 @@ for(i=0;i<strlen(chaine);i++){
      } if(cc==0) printf(" C EST VIDE!");
     printf("\n\n | operateurs | ");
       for(j=0;j<oc;j++){
-        printf("%c ",operators[j]);
+        if((operators[j]==':')&&(operators[j+1]=='=')){
+            printf(" := ");
+        }/* else if(operators[j]=='='){
+            printf(" = ");
+        } */else if(operators[j]=='+'){
+            printf(" + ");
+        } else if(operators[j]=='-'){
+            printf(" - ");
+        }
       } if(oc==0) printf(" C EST VIDE!");
       printf("\n\n | mot cle | ");
-      for(j=0;j<mc;j++){
-        printf("%c ",motcle[j]);
+      for(i=0;i<mc;i++){//then end
+            if((motcle[i]=='p')&&(motcle[i+1]=='r')&&(motcle[i+2]=='o')&&(motcle[i+3]=='g')&&(motcle[i+4]=='r')&&(motcle[i+5]=='a')&&(motcle[i+6]=='m')){
+                printf(" program ");
+            } else if((motcle[i]=='v')&&(motcle[i+1]=='a')&&(motcle[i+2]=='r')){
+                printf(" var ");
+            }else if ((chaine[i]=='i')&&(chaine[i+1]=='n')&&(chaine[i+2]=='t')){
+            printf(" int ");
+            }else if ((motcle[i]=='i')&&(motcle[i+1]=='f')){
+            printf(" if ");
+            }else if ((motcle[i]=='b')&&(motcle[i+1]=='e')&&(motcle[i+2]=='g')&&(motcle[i+3]=='i')&&(motcle[i+4]=='n')){
+                printf(" begin ");
+            } else if ((motcle[i]=='e')&&(motcle[i+1]=='l')&&(motcle[i+2]=='s')&&(motcle[i+3]=='e')){
+                printf(" else ");
+            } else if ((motcle[i]=='r')&&(motcle[i+1]=='e')&&(motcle[i+2]=='t')&&(motcle[i+3]=='u')&&(motcle[i+4]=='r')&&(motcle[i+5]=='n')){
+                printf(" return ");
+            }else if ((motcle[i]=='t')&&(motcle[i+1]=='h')&&(motcle[i+2]=='e')&&(motcle[i+3]=='n')){
+                printf(" then ");
+            }else if ((motcle[i]=='e')&&(motcle[i+1]=='n')&&(motcle[i+2]=='d')){
+                printf(" end ");
+            }
+
+
       } if(mc==0) printf(" C EST VIDE!");
        printf("\n\n | separateurs | ");
       for(j=0;j<sp;j++){
